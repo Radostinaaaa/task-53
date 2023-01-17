911e2469000dcb1365e59789a705b38bec009983
Radostina Emilova <118113591+Radostinaaaa@users.noreply.github.com>
2023-01-17 16:36:20 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,9 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-
+  const price=document.getElementsByClassName('price');
+  var elements = document.getElementsByClassName('product');
+  for(var i=0; i<elements.length; i++) { 
+  elements[i].setAttribute('data-price', price[0].innerHTML);
+  }
 });
